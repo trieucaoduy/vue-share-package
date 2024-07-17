405dcbd9c0cc21bfc46b293c3a270b8faf92a234
trieucd <trieucd@runsystem.net>
2024-07-17 11:36:38 +0700

    chore: update style

--- core-module/src/App.vue
+++ core-module/src/App.vue
@@ -25,17 +25,10 @@ export default defineComponent({
   />
 </template>
 
-<style scoped>
-.logo {
-  height: 6em;
-  padding: 1.5em;
-  will-change: filter;
-  transition: filter 300ms;
-}
-.logo:hover {
-  filter: drop-shadow(0 0 2em #646cffaa);
-}
-.logo.vue:hover {
-  filter: drop-shadow(0 0 2em #42b883aa);
+<style scoped lang="scss">
+.pkg-select__wrapper {
+  :deep(.p-select) {
+    width: 200px;
+  }
 }
 </style>
